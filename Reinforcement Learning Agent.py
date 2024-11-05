a71f2d2c32b48b86650aff007c59796f3a50fffa
Matt2939 <matthew.casey@marquette.edu>
2024-11-05 16:59:06 -0600

    Part 2 code

---
+++ Reinforcement Learning Agent.py
@@ -0,0 +1,111 @@
+#Reinforcement Learning Agent
+'''You need to run the folowing commands in the terminal '''
+# pip install matplotlib
+# pip install "stable-baselines3"
+
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import gym
+from kaggle_environments import make, evaluate
+from gym import spaces
+
+import torch as th
+import torch.nn as nn
+
+from stable_baselines3 import PPO 
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+
+class ConnectFourGym(gym.Env):
+    def __init__(self, agent2="random"):
+        ks_env = make("connectx", debug=True)
+        self.env = ks_env.train([None, agent2])
+        self.rows = ks_env.configuration.rows
+        self.columns = ks_env.configuration.columns
+        # Learn about spaces here: http://gym.openai.com/docs/#spaces
+        self.action_space = spaces.Discrete(self.columns)
+        self.observation_space = spaces.Box(low=0, high=2, 
+                                            shape=(1,self.rows,self.columns), dtype=int)
+        # Tuple corresponding to the min and max possible rewards
+        self.reward_range = (-10, 1)
+        # StableBaselines throws error if these are not defined
+        self.spec = None
+        self.metadata = None
+    def reset(self):
+        self.obs = self.env.reset()
+        return np.array(self.obs['board']).reshape(1,self.rows,self.columns)
+    def change_reward(self, old_reward, done):
+        if old_reward == 1: # The agent won the game
+            return 1
+        elif done: # The opponent won the game
+            return -1
+        else: # Reward 1/42
+            return 1/(self.rows*self.columns)
+    def step(self, action):
+        # Check if agent's move is valid
+        is_valid = (self.obs['board'][int(action)] == 0)
+        if is_valid: # Play the move
+            self.obs, old_reward, done, _ = self.env.step(int(action))
+            reward = self.change_reward(old_reward, done)
+        else: # End the game and penalize agent
+            reward, done, _ = -10, True, {}
+        return np.array(self.obs['board']).reshape(1,self.rows,self.columns), reward, done, _
+
+# Neural network for predicting action values
+class CustomCNN(BaseFeaturesExtractor):
+    
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int=128):
+        super(CustomCNN, self).__init__(observation_space, features_dim)
+        # CxHxW images (channels first)
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with th.no_grad():
+            n_flatten = self.cnn(
+                th.as_tensor(observation_space.sample()[None]).float()
+            ).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.linear(self.cnn(observations))
+
+policy_kwargs = dict(
+    features_extractor_class=CustomCNN,)
+        
+def agent1(obs, config):
+    # Use the best model to select a column
+    col, _ = model.predict(np.array(obs['board']).reshape(1, 6,7))
+    # Check if selected column is valid
+    is_valid = (obs['board'][int(col)] == 0)
+    # If not valid, select random move. 
+    if is_valid:
+        return int(col)
+    else:
+        return random.choice([col for col in range(config.columns) if obs.board[int(col)] == 0])
+
+
+#Main
+
+env = ConnectFourGym(agent2="random")
+
+model = PPO("CnnPolicy", env, policy_kwargs=policy_kwargs, verbose=0)
+
+model.learn(total_timesteps=10)
+# Create the game environment
+#env = make("connectx")
+
+# Two random agents play one game round
+#env.run([agent1, "random"])
+
+# Show the game
+#env.render(mode="ipython")
